31858427e20f1fb4e346e21cf40ee6a069c1829c
Mulenga <mulenga-kangwa@hotmail.com>
2020-08-08 01:09:49 +0100

    Initial commit - list comprehensions

---
+++ 99EvenNumbersinaList.py
@@ -0,0 +1,8 @@
+
+'''st=[x for x in range(2,21,2)]
+print(lst)'''
+
+lst=[x for x in range(1,21) if x%2==0]
+print(lst)
+
+#Both of the scripts above achieve the same resuult: that is, list the even numbers between 2 and 21.
